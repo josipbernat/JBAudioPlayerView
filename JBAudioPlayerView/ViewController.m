ed85fdf68ddfa69f4eb9866c501951c61d441147
Josip Bernat <josip.bernat@clover-studio.com>
2015-01-22 15:27:12 +0100

    Readme.md updated

--- JBAudioPlayerView/ViewController.m
+++ JBAudioPlayerView/ViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet JBAudioPlayerView *autoLayoutAudioPlayerView;
 @property (weak, nonatomic) IBOutlet JBAudioPlayerView *noAutoLayoutAudioPlayerView;
+@property (strong, nonatomic) JBAudioPlayerView *fromCode;
 
 @end
 
@@ -23,6 +24,26 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.noAutoLayoutAudioPlayerView.useAutoLayout = NO;
+    
+    JBAudioPlayerView *fromCode = [[JBAudioPlayerView alloc] initWithFrame:CGRectMake(CGRectGetMinX(self.noAutoLayoutAudioPlayerView.frame),
+                                                                                      CGRectGetMaxY(self.noAutoLayoutAudioPlayerView.frame) + 30.0f,
+                                                                                      CGRectGetWidth(self.noAutoLayoutAudioPlayerView.frame),
+                                                                                      CGRectGetHeight(self.noAutoLayoutAudioPlayerView.frame))];
+    fromCode.backgroundColor = [UIColor yellowColor];
+    fromCode.useAutoLayout = NO;
+    [self.view addSubview:fromCode];
+    
+    self.fromCode = fromCode;
+}
+
+- (void)viewDidLayoutSubviews {
+
+    [super viewDidLayoutSubviews];
+    
+    self.fromCode.frame = CGRectMake(CGRectGetMinX(self.noAutoLayoutAudioPlayerView.frame),
+                                     CGRectGetMaxY(self.noAutoLayoutAudioPlayerView.frame) + 30.0f,
+                                     CGRectGetWidth(self.noAutoLayoutAudioPlayerView.frame),
+                                     CGRectGetHeight(self.noAutoLayoutAudioPlayerView.frame));
 }
 
 - (void)didReceiveMemoryWarning {
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # JBAudioPlayerView
 Simple view component for playing audio files in iOS. Suitable for usage in UITableView and UICollectionView.
+
+![alt tag](http://url/to/img.png)
+
+## USAGE
+You can use it from Storyboards, XIBs or directly from code. It uses AutoLayou by default but if you wish to turn it off just set useAutoLayout to NO.
+
+### `GET` Request
+```objective-c
+JBAudioPlayerView *fromCode = [[JBAudioPlayerView alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 200.0f, 50.0f)];
+fromCode.backgroundColor = [UIColor yellowColor];
+fromCode.useAutoLayout = NO;
+[self.view addSubview:fromCode];
+```
+
